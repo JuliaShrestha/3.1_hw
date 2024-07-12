187a3debb5d6ad298875134bfbe155d98b87d16f
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-12 23:58:12 +0300

    Data types

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const userName = "Dzhulia";
+console.log(userName);
+console.log(typeof userName);
+
+const age = 20;
+console.log(age);
+console.log(typeof age);
+
+const bigNumber = 125478556n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+const hasCar = false;
+console.log(hasCar);
+console.log(typeof hasCar);
+
+const isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+const emptyValue = null;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+let someVariable;
+console.log(someVariable);
+console.log(typeof someVariable);
+
+const uniqueID = Symbol('description');
+console.log(uniqueID);
+console.log(typeof uniqueID);
+
+let user = {
+    name: 'Alice',
+    age: 25,
+    email: "alice@gmail.com"
+};
+console.log(user);
+console.log(typeof user);
